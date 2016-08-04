ba91bdf55a32fbacf2e1cf7d7e0a67ca2e32410e
Sophia Varella <sophiavarella@gmail.com>
2016-08-04 14:12:56 -0700

    Insert questions for data gathering from new employees

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "What year were you born?"
+birth_year = gets.chomp.to_i
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (yess/no)"
+likes_garlic = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance? (yes/no)"
+insurance = gets.chomp
