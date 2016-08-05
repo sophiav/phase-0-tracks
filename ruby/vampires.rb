4b73aa1e5e26036ffd23616855b9b4eb1cf2d716
Sophia Varella <sophiavarella@gmail.com>
2016-08-04 18:33:47 -0700

    Update vampire-detecting logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -13,39 +13,27 @@ likes_garlic = gets.chomp
 puts "Would you like to enroll in the company's health insurance? (yes/no)"
 insurance = gets.chomp
 
+
+
 current_year = 2016
 vampire = ""
 
-# if (age == (current_year - birth_year)) && ((likes_garlic == "yes") || (insurance == "yes"))
-#   vampire = "Probably not a vampire"
-# elsif (age != (current_year - birth_year)) && ((likes_garlic == "no") || (insurance == "no"))
-#   vampire = "Probably a vampire"
-# elsif (age != (current_year - birth_year)) && (likes_garlic == "no") && (insurance == "no")
-#   vampire = "Almost certainly a vampire!"
-# elsif name == "Drake Cula" || name == "Tu Fang"
-#   vampire = "Definitely a vampire!!"
-# else
-#   vampire = "Results inconclusive "
-# end
 
-# puts "#{name} is: #{vampire}"
+if (age == (current_year - birth_year)) && ((likes_garlic == "yes") || (insurance == "yes"))
+  vampire = "Probably not a vampire"
+end
+if (age != (current_year - birth_year)) && ((likes_garlic == "no") || (insurance == "no"))
+  vampire = "Probably a vampire"
+end
+if (age != (current_year - birth_year)) && (likes_garlic == "no") && (insurance == "no")
+  vampire = "Almost certainly a vampire!"
+end
+if name == "Drake Cula" || name == "Tu Fang"
+  vampire = "Definitely a vampire!!"
+end
+if vampire == ""
+  vampire = "Results inconclusive"
+end
 
+puts "#{name} is: #{vampire}"
 
-
-outcome = ""
-
-vampire2 = case outcome
-
-  when (age == (current_year - birth_year)) && ((likes_garlic == "yes") || (insurance == "yes"))
-    outcome = "Probably not a vampire"
-  when (age != (current_year - birth_year)) && ((likes_garlic == "no") || (insurance == "no"))
-    outcome = "Probably a vampire"
-  when (age != (current_year - birth_year)) && (likes_garlic == "no") && (insurance == "no")
-    outcome = "Almost certainly a vampire!"
-  when name == "Drake Cula" || name == "Tu Fang"
-    outcome = "Definitely a vampire!!"
-  else
-     outcome = "Results inconclusive "
-  end 
-
-puts vampire2
