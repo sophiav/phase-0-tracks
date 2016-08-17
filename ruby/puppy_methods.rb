9dd998f16af64b9afb649e0a0ec65ddce751da45
Sophia Varella <sophiavarella@gmail.com>
2016-08-16 19:31:57 -0700

    Create more instance methods and add driver code to test them

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,45 @@ class Puppy
     toy
   end
 
+  def speak(integer)
+    puts "Whoof!" * integer
+  end
+
+  def roll_over
+    puts "*roll over*"
+  end
+
+  def dog_years(integer)
+    puts dog_age = integer * 7
+    dog_age
+  end
+
+  def swim
+    puts "*Splash!!*"
+  end
+
+  def initialize
+    puts "Initializing a new puppy instance ..."
+  end
+
 end
+
+
+# Add driver code to test instance methods for class Puppy:
+# storm = Puppy.new
+# storm.fetch("ball")
+# storm.speak(3)
+# storm.roll_over
+# storm.dog_years(2)
+# storm.swim
+
+
+
+
+
+
+
+
+
+
 
