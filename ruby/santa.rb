caf1816cb0d93d1e4ca8e45b1f714ae5a58e679f
Sophia Varella <sophiavarella@gmail.com>
2016-08-17 13:17:43 -0700

    Add instance variables to the Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,17 +1,24 @@
 class Santa
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
   end
+
+  @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+  @age = 0
 
   def speak
     puts "Ho, ho, ho! Haaaappy Holidays!!"
   end
 
-  def eat_mil_and_cookies(cookie="choc-chip")
+  def eat_milk_and_cookies(cookie)
     puts "That was a good #{cookie} cookie!!"
   end
+
 end
 
-jeff = Santa.new
-jeff.speak
-jeff.eat_mil_and_cookies("blueberry-yogurt")
+# jeff = Santa.new
+# jeff.speak
+# jeff.eat_milk_and_cookies("blueberry-yogurt")
+
