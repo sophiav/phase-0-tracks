d57f8990d95755def9402e5c37a4351ed9dacf94
Sophia Varella <sophiavarella@gmail.com>
2016-08-18 12:56:11 -0700

    Create user interface

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,30 +1,28 @@
+# Release 1: Design class Pokemon
 =begin
-Design class Pokemon:
-
-Attributes (state)
+Attributes (state):
   - Name: varies
   - Weight: varies
   - Height: varies
   - Type: varies
   - Location: fixed
 
-
-Behaviors
+Behaviors:
   - Evolve
   - Fight: takes argument power
   - Heal: takes argument integer
   - About
-  
 =end
 
-# Create class Pokemon
+# Release 2: Create class Pokemon
 class Pokemon
   attr_reader :type
-  attr_accessor :name, :location, :weight, :height
-  def initialize(name, location, type)
+  attr_accessor :name, :location, :weight, :height, :weight
+  def initialize(name, location, type, height, weight)
+    puts 'Initializing pokemon instance...'
     @name = name
-    @weight = 1.5
-    @height = 1
+    @weight = weight
+    @height = height
     @type = type
     @location = location
   end 
@@ -37,7 +35,7 @@ class Pokemon
     puts "#{@name} used: #{power}" 
   end
 
-  def heal(integer_points)
+  def heal(integer_points=15)
     puts "#{@name}'s health restored by #{integer_points} points"
   end 
 
@@ -45,8 +43,92 @@ class Pokemon
     puts "Name: #{@name}"
     puts "Type: #{@type}"
     puts "Weight: #{@weight} kgs"
-    puts "Height: #{@height} meters"
+    puts "Height: #{@height} m"
     puts "Location: #{@location}"
   end
-
 end
+
+# Driver code for Release 2:
+# pokemon = Pokemon.new("Aerodactyl", "Mission", "flying", 1.7)
+# p pokemon
+# pokemon.evolve
+# pokemon.fight("steel wing")
+# pokemon.heal
+# pokemon.about
+
+
+# Release 3: User Interface
+puts "Time to fill your pokedex! How many pokemons are you registering today?"
+pokemon_number = gets.chomp.to_i
+
+pokedex = []
+
+count = 0
+until count == pokemon_number
+  puts "What is your pokemon's name?"
+  name = gets.chomp
+
+  puts "What was the location your pokemon was caught?"
+  location = gets.chomp
+
+  puts "What type is your pokemon (i.e. water, fire, bug, dragon etc.)?"
+  type = gets.chomp
+
+  puts "What's your pokemon's height?"
+  height = gets.chomp.to_f
+
+  puts "What's your pokemon's weight?"
+  weight = gets.chomp.to_f
+
+  pokemon = Pokemon.new(name, location, type, height, weight)
+
+  pokedex << pokemon
+
+  count += 1
+end
+
+p pokedex
+
+puts "Good job! You registered the following pokemons today:"
+pokedex.each do |pokemon|
+  puts "Name: #{pokemon.name}"
+  puts "Location #{pokemon.location}"
+  puts "Type: #{pokemon.type}"
+  puts "Height: #{pokemon.height}"
+  puts "Weight: #{pokemon.weight}"
+  pokemon.evolve
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
