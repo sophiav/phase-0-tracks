09e86b6bb39b70143af0e23e0d675d6f695cab8a
Sophia Varella <sophiavarella@gmail.com>
2016-08-18 11:55:21 -0700

    Create a new class Pokemon that includes atrributes and methods

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,17 +1,52 @@
 =begin
-Design for class Pokemon:
+Design class Pokemon:
 
 Attributes (state)
   - Name: varies
   - Weight: varies
   - Height: varies
   - Type: varies
+  - Location: fixed
 
 
 Behaviors
   - Evolve
-  - Power Up
-  - Fight: takes argument skill
+  - Fight: takes argument power
   - Heal: takes argument integer
+  - About
   
 =end
+
+# Create class Pokemon
+class Pokemon
+  attr_reader :type
+  attr_accessor :name, :location, :weight, :height
+  def initialize(name, location, type)
+    @name = name
+    @weight = 1.5
+    @height = 1
+    @type = type
+    @location = location
+  end 
+
+  def evolve
+    puts "#{@name} has evolved!!!"
+  end 
+
+  def fight(power)
+    puts "#{@name} used: #{power}" 
+  end
+
+  def heal(integer_points)
+    puts "#{@name}'s health restored by #{integer_points} points"
+  end 
+
+  def about
+    puts "Name: #{@name}"
+    puts "Type: #{@type}"
+    puts "Weight: #{@weight} kgs"
+    puts "Height: #{@height} meters"
+    puts "Location: #{@location}"
+  end
+
+end
