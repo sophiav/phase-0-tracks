c7013bd43f9ac081bab2627e217cd7a525bdc5ab
Sophia Varella <sophiavarella@gmail.com>
2016-08-18 11:16:43 -0700

    Pseudocode a design for class Pokemon: add state and behaviors

---
+++ ruby/solo.rb
@@ -0,0 +1,17 @@
+=begin
+Design for class Pokemon:
+
+Attributes (state)
+  - Name: varies
+  - Weight: varies
+  - Height: varies
+  - Type: varies
+
+
+Behaviors
+  - Evolve
+  - Power Up
+  - Fight: takes argument skill
+  - Heal: takes argument integer
+  
+=end
