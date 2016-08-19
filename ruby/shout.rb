13c2dce35ace3a1fb164fdb87f1ccb3e6d4d047b
Sophia Varella <sophiavarella@gmail.com>
2016-08-18 18:29:10 -0700

    Create a new mixin module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,27 @@
+# # Declaring a Module called Shout
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#     words.upcase + "!!!" + " :)"
+#   end
+# end
+
+# # Driver code
+# p Shout.yell_angrily("this program does not work")
+# p Shout.yell_happily("Yey. I found the bug in my program!")
+
+
 # Declaring a Module called Shout
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_angrily(words)
+    words + "!!!" 
   end
 
-  def self.yell_happily(words)
+  def yell_happily(words)
     words.upcase + "!!!" + " :)"
   end
 end
 
-# Driver code
-p Shout.yell_angrily("this program does not work")
-p Shout.yell_happily("Yey. I found the bug in my program!")
