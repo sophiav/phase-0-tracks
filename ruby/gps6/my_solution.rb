470489443fce27a4965b298ca16a3e4b8fc4605c
Sophia Varella <sophiavarella@gmail.com>
2016-08-22 19:31:50 -0700

    Refactor predicted_deaths method: remove repitition from having the number of deaths calculation performed at the end using the multiply factor we set for each population_density

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -26,17 +26,23 @@ class VirusPredictor
   def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      multiply_factor = 0.4
+      # number_of_deaths = (@population * 0.4).floor
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      multiply_factor = 0.4
+      # number_of_deaths = (@population * 0.3).floor
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      multiply_factor = 0.4
+      # number_of_deaths = (@population * 0.2).floor
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      multiply_factor = 0.4
+      # number_of_deaths = (@population * 0.1).floor
     else
-      number_of_deaths = (@population * 0.05).floor
+      multiply_factor = 0.4
+      # number_of_deaths = (@population * 0.05).floor  
     end
 
+    number_of_deaths = (@population * multiply_factor).floor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
