b406f1d4db4b62f2458a86fa50e3d7e16e9e34a5
Sophia Varella <sophiavarella@gmail.com>
2016-08-22 19:27:07 -0700

    Refactor virus_effects, predicted_deaths and speed_of_spread methods - removing the requirement to add parameters in their individual method definitions

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -17,13 +17,13 @@ class VirusPredictor
   end
 
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -41,7 +41,7 @@ class VirusPredictor
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
