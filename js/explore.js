c0f1aa2693e3115d0e4d76c6483e0574fa794d9e
Sophia Varella <sophiavarella@gmail.com>
2016-08-27 15:24:39 -0700

    Add pseudocode for the method reverseString

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+// Pseudocode for function: reverseString
+// Input: string
+// Output: reversed string
+  // Write a function reverseString that takes 1 parameter, a string
+  // Split the string to form an array of characters
+  // Call .reverse on the array to reverse the order of all the characters
+  // Join the modified array into a string.
+  // Finally, return the new string 
+
+
