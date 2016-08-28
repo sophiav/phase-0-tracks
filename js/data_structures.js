77c92a17e6ad6249e235a9e59d479f9965434281
Sophia Varella <sophiavarella@gmail.com>
2016-08-27 20:05:00 -0700

    Create colors and names arrays

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+// Release 0:
+var colors = ["blue", "green", "purple", "pink"];
+var names = ["Ed", "Cali", "John", "Storm"];
+
+colors.push("red");
+names.push("Jack");
+
+console.log(colors);
+console.log(names);
