95d7495f1443741a647310255bcae19560b6087c
Sophia Varella <sophiavarella@gmail.com>
2016-08-27 20:25:18 -0700

    Create a horses object and mapped the names to the colors from the respective arrays

--- js/data_structures.js
+++ js/data_structures.js
@@ -7,3 +7,11 @@ names.push("Jack");
 
 console.log(colors);
 console.log(names);
+
+var horses = {};
+console.log(horses);
+
+for (var i = 0; i < colors.length; i++) {
+  horses[names[i]] = colors[i];
+};
+console.log(horses);
