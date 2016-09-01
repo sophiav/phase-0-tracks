cea076e8c560e5de3db6411510b9fc96422f36ec
Sophia Varella <sophiavarella@gmail.com>
2016-08-31 20:57:01 -0700

    Create a table (pokedex) in the pokemon database

--- databases/pokemon_maker/pokemon.rb
+++ databases/pokemon_maker/pokemon.rb
@@ -7,3 +7,17 @@ require 'faker'
 
 # create SQLite3 database
 db = SQLite3::Database.new("pokemon.db")
+
+# create a pokemons table command
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS pokedex(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    location VARCHAR(255),
+    weight INT,
+    height INT
+  )
+SQL
+
+# create a pokemons table (if it's not there already)
+db.execute(create_table_cmd)
