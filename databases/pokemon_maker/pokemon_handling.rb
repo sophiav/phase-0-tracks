b38ab200030360c0a6af5e9a9133a9561740fcdf
Sophia Varella <sophiavarella@gmail.com>
2016-09-02 09:33:42 -0700

    Pseudocode the initial intentions

---
+++ databases/pokemon_maker/pokemon_handling.rb
@@ -0,0 +1,16 @@
+# GOALS:
+# 1. CREATE, RETRIEVE AND MANIPULATE A POKEMON LIST 
+# 2. USE RELATIONAL DATA IN A RUBY PROGRAM
+
+# require gems
+
+# create a Pokedex Class
+  # create a pokedex table command
+  # create a pokedex table (if it's not already there)
+  # add pokemon to the pokedex
+  # add methods to include some behavior characteristics
+  # print pokedex information in a nice format
+
+# Include Driver Code
+
+# Provide a User Interface so the player can interact with the pokemons
