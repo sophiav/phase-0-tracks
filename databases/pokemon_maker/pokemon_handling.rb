aff3c5ebc69256ecd2b9cf6a72fffc19329bb244
Sophia Varella <sophiavarella@gmail.com>
2016-09-02 10:58:45 -0700

    Include user interface logic

--- databases/pokemon_maker/pokemon_handling.rb
+++ databases/pokemon_maker/pokemon_handling.rb
@@ -18,26 +18,6 @@ class Pokedex
     create_table
   end
 
-  def create_table
-    # create a pokedex table command
-    create_table_cmd = <<-SQL
-      CREATE TABLE IF NOT EXISTS pokedex(
-        id INTEGER PRIMARY KEY,
-        name VARCHAR(255),
-        location VARCHAR(255),
-        cp INT,
-        evolve BOOLEAN
-      )
-    SQL
-  
-    # create a pokedex table (if it's not already there)
-    @db.execute(create_table_cmd)
-
-    # since we are running this program multiple times, if the table is already there,
-    # then clear all the values inside it so was can start fresh with no data
-    @db.execute('DELETE from pokedex');
-  end
-
   # add pokemon to the pokedex
   def create_pokemons(name, location, cp, evolve)
     @db.execute("INSERT INTO pokedex (name, location, cp, evolve) VALUES (?, ?, ?, ?)", [name, location, cp, evolve])
@@ -70,16 +50,38 @@ class Pokedex
     pokedex = @db.execute("SELECT * FROM pokedex")
 
     puts "_" * 50
-    puts "Name, Location, CP, Evolve" # print heading for the table
+    puts "ID | Name | Location | CP | Evolve?" # print heading for the table
     pokedex.each do |pokemon|
-      puts "#{pokemon['name']}, #{pokemon['location']}, #{pokemon['cp']}, #{pokemon['evolve']}"
+      puts "#{pokemon['id']} | #{pokemon['name']} | #{pokemon['location']} | #{pokemon['cp']} | #{pokemon['evolve']}"
     end
     
     puts "_" * 50
   end
+
+  private
+  def create_table
+    # create a pokedex table command
+    create_table_cmd = <<-SQL
+      CREATE TABLE IF NOT EXISTS pokedex(
+        id INTEGER PRIMARY KEY,
+        name VARCHAR(255),
+        location VARCHAR(255),
+        cp INT,
+        evolve BOOLEAN
+      )
+    SQL
+  
+    # create a pokedex table (if it's not already there)
+    @db.execute(create_table_cmd)
+
+    # since we are running this program multiple times, if the table is already there,
+    # then clear all the values inside it so was can start fresh with no data
+    @db.execute('DELETE from pokedex');
+  end
 end
 
-# Include Driver Code
+
+# CREATING NEW POKEMON IN THE POKEDEX TABLE USING FAKER GEM
 pokedex = Pokedex.new
 
 # create 20 pokemons for the pokedex
@@ -92,4 +94,27 @@ pokedex = Pokedex.new
   pokedex.create_pokemons(name, location, cp, evolve)
 end
 
-# Provide a User Interface so the player can interact with the pokemons
+
+# USER INTERFACE
+# Provide a User Interface so the player can interact with the pokemon in pokedex
+pokedex.print_pokedex
+pokemon_choice = ''
+while pokemon_choice != 'Done'
+  puts "Select a pokemon from your pokedex, or type 'done' if you are finished."
+  pokemon_choice = gets.chomp.capitalize
+  break if pokemon_choice == 'Done'
+
+  puts "Would you like to power-up your pokemon, evolve it or transfer it to the professor? Type 'power-up', evolve', or 'transfer'"
+  action_choice = gets.chomp
+
+  if action_choice == "evolve"
+    pokedex.evolve(pokemon_choice)
+  elsif action_choice == "power-up"
+    pokedex.power_up(pokemon_choice)
+  else action_choice == "transfer"
+    pokedex.transfer(pokemon_choice)
+  end
+
+  puts "Sweet! Checkout your updated pokedex:"
+  pokedex.print_pokedex
+end
