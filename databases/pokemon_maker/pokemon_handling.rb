0871a3c9818b8409021cf325c4287b6c49a2e9d0
Sophia Varella <sophiavarella@gmail.com>
2016-09-02 09:52:27 -0700

    Create the pokedex table

--- databases/pokemon_maker/pokemon_handling.rb
+++ databases/pokemon_maker/pokemon_handling.rb
@@ -15,8 +15,26 @@ class Pokedex
     # when we call .new on the Database class (change from array to a hash)
     @db.results_as_hash = true
   end
-  # create a pokedex table command
-  # create a pokedex table (if it's not already there)
+
+  def create_table
+    # create a pokedex table command
+    create_table_cmd = <<-SQL
+      CREATE TABLE IF NOT EXISTS pokedex(
+        id INTEGER PRIMARY KEY,
+        name VARCHAR(255),
+        location VARCHAR(255),
+        cp INT,
+        evolve BOOLEAN
+      )
+    SQL
+  
+    # create a pokedex table (if it's not already there)
+    @db.execute(create_table_cmd)
+
+    # since we are running this program multiple times, if the table is already there,
+    # then clear all the values inside it so was can start fresh with no data
+    @db.execute('DELETE from pokedex');
+  end
   # add pokemon to the pokedex
   # add methods to include some behavior characteristics
   # print pokedex information in a nice format
