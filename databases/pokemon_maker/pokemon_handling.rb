78b6b287fbe096095db7baf81b95ba5068b63842
Sophia Varella <sophiavarella@gmail.com>
2016-09-02 10:19:22 -0700

    Add pokemon to the pokedex

--- databases/pokemon_maker/pokemon_handling.rb
+++ databases/pokemon_maker/pokemon_handling.rb
@@ -14,6 +14,8 @@ class Pokedex
     # change the data structure type of each new instance the SQLite gem creates
     # when we call .new on the Database class (change from array to a hash)
     @db.results_as_hash = true
+
+    create_table
   end
 
   def create_table
@@ -35,11 +37,28 @@ class Pokedex
     # then clear all the values inside it so was can start fresh with no data
     @db.execute('DELETE from pokedex');
   end
+
   # add pokemon to the pokedex
+  def create_pokemons(name, location, cp, evolve)
+    @db.execute("INSERT INTO pokedex (name, location, cp, evolve) VALUES (?, ?, ?, ?)", [name, location, cp, evolve])
+  end
+  
   # add methods to include some behavior characteristics
   # print pokedex information in a nice format
 end
 
 # Include Driver Code
+pokedex = Pokedex.new
+
+
+# create 20 pokemons for the pokedex
+20.times do
+  name = Faker::Pokemon.name
+  location = Faker::Pokemon.location
+  cp = Faker::Number.number(3)
+  evolve = 'false'
+
+  pokedex.create_pokemons(name, location, cp, evolve)
+end
 
 # Provide a User Interface so the player can interact with the pokemons
