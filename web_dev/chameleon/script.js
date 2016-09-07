9c4e25dcdcb3b2d7f10750904106cd7a0e5eee74
Sophia Varella <sophiavarella@gmail.com>
2016-09-07 16:25:31 -0700

    Add comments from practice

--- web_dev/chameleon/script.js
+++ web_dev/chameleon/script.js
@@ -1,10 +1,14 @@
-console.log("The script is running!");
+console.log("The script is running");
 
-function addPinkBorder(event) {
-  console.log("click happened! here's the click event:");
+// Create an event-listener
+//1. Add a function that selects the photo objects and add a border
+function addPinkBorder (event) {
+  console.log("click happened! here's the click event:")
   console.log(event);
   event.target.style.border = "2px solid pink";
 }
 
+
+//2. Add an event listener
 var photo = document.getElementById("lizard-photo");
 photo.addEventListener("click", addPinkBorder);
