5a5b3241a776b25ffe2a4e8c8e94009266c00e69
Sophia Varella <sophiavarella@gmail.com>
2016-10-04 16:35:40 -0700

    Update the TodoList class so that tests pass

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,24 +1,45 @@
 class TodoList
-  attr_accessor :todo_list
-
   def initialize(array)
-    @todo_list = ["do the dishes", "mow the lawn"]
+    @array = array
+  end
+
+  def get_items
+    @array
   end
 
   def add_item(item)
-    @todo_list.push(item)
+    @array.push(item)
   end
 
   def delete_item(item)
-    @todo_list.delete(item)
+    @array.delete(item)
   end
 
   def get_item(index)
-    @todo_list[index]
-      end
-
-  def get_items
-    @todo_list
+    @array[index]
   end
-
 end
+
+# class TodoList
+#   attr_accessor :todo_list
+
+#   def initialize(array)
+#     @todo_list = ["do the dishes", "mow the lawn"]
+#   end
+
+#   def add_item(item)
+#     @todo_list.push(item)
+#   end
+
+#   def delete_item(item)
+#     @todo_list.delete(item)
+#   end
+
+#   def get_item(index)
+#     @todo_list[index]
+#       end
+
+#   def get_items
+#     @todo_list
+#   end
+# end
